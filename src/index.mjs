d58f97cb6773f70b88f80660ed2a1b16b5adf9ee
charlesanderson25 <charles_spfc@hotmail.com>
2023-08-19 18:42:27 -0300

    Add file with fs module

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import fs from "fs";
+
+const conteudo = "Texto do arquivo teste!";
+
+try {
+  fs.writeFileSync("teste.txt", conteudo);
+  console.log("Arquivo criado com sucesso!");
+} catch (error) {
+  console.error("Erro ao criar arquivo", error);
+}
