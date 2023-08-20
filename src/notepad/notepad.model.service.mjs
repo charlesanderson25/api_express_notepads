666fbe355a93026ae38eb277e8568e70bf1f0714
charles <charles_spfc@hotmail.com>
2023-08-20 14:27:16 -0300

    add createNotepad

--- src/notepad/notepad.model.service.mjs
+++ src/notepad/notepad.model.service.mjs
@@ -1,4 +1,12 @@
-function createNotepad() {}
+import fs from "fs";
+
+async function createNotepad(path, notepad) {
+  const notepadString = JSON.stringify(notepad, null, 2);
+  await fs.promises.writeFile(path, notepadString);
+}
+
 function readNotepad() {}
 function updateNotepad() {}
 function deleteNotepad() {}
+
+export default createNotepad;
