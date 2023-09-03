37345a3acd33c0440812dbf077d26887bc14062a
charlesanderson25 <charles_spfc@hotmail.com>
2023-09-03 15:50:08 -0300

    Add read, creat, delete, patch

--- src/notepad/notepad.controller.mjs
+++ src/notepad/notepad.controller.mjs
@@ -1,3 +1,45 @@
 import express from "express";
 
-export const notepadController = express.Router();
+const notepadController = express.Router();
+
+//Read Notepad
+notepadController.get("/:id", async (req, res) => {
+  const notepadId = req.params.id;
+  const notepad = await readNotepad(`data/notepads/${notepadId}.json`);
+  res.status(200).json(notepad);
+});
+
+//Create Notepad
+
+notepadController.post("/", async (req, res) => {
+  const nextNotepad = req.body;
+  const notepadPath = `data/notepads/${nextNotepad.id}.json`;
+
+  try {
+    await createNotepad(notepadPath, nextNotepad);
+    console.log(`Notepad criado com sucesso em ${notepadPath}`);
+    res.status(201).json(nextNotepad);
+  } catch (error) {
+    console.error(`Erro ao criar o notepad: ${error}`);
+    res.status(500).json({ error: "Erro ao criar o notepad" });
+  }
+});
+
+notepadController.delete("/:id", async (req, res) => {
+  const notepadId = req.params.id;
+  const path = `data/notepads/${notepadId}.json`;
+  const notepad = await readNotepad(path);
+  await deleteNotepad(path);
+  res.status(200).json(notepad);
+});
+
+notepadController.patch("/:id", async (req, res) => {
+  const notepadId = req.params.id;
+  const partialNotepad = req.body;
+  const path = `data/notepads/${notepadId}.json`;
+  await updateNotepad(path, partialNotepad);
+  const notepad = await readNotepad(path);
+  res.status(200).json(notepad);
+});
+
+export default notepadController;
