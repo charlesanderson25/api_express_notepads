183b8adae636c7b8cade1ff79a18bf8200cd0baf
charlesanderson25 <charles_spfc@hotmail.com>
2023-09-09 10:05:06 -0300

    Rewrite new code

--- src/notepad/notepad.seed.mjs
+++ src/notepad/notepad.seed.mjs
@@ -1,3 +1,30 @@
+// import {
+//   createNotepad,
+//   deleteNotepad,
+//   listNotepads,
+//   readNotepad,
+//   updateNotepad,
+// } from "./notepad.model.service.mjs";
+
+// const limit = 10;
+
+// async function notepadSeed() {
+//   console.log("Iniciando Seed...");
+
+//   for (let i = 0; i < limit; i++) {
+//     const notepad = await createNotepad({
+//       title: "Titulo Seed",
+//       subtitle: "Subtitulo Seed",
+//       content: "Conteúdo test do Seed",
+//     });
+//     console.log(`Novo notepad criado com id: ${notepad.id}`);
+//   }
+//   console.log("Seed realizando com sucesso!");
+//   //   console.log(`Resultado do Seed: ${createNotepad}`);
+// }
+
+// notepadSeed();
+
 import {
   createNotepad,
   deleteNotepad,
@@ -6,11 +33,21 @@ import {
   updateNotepad,
 } from "./notepad.model.service.mjs";
 
-console.log("Seed");
+const limit = 5;
 
-createNotepad({
-  title: "Titulo Seed",
-  subtitle: "Subtitulo Seed",
-  content: "Conteúdo test do Seed",
-  createdAt: "2023/09/09",
-});
+async function notepadSeed() {
+  console.log("Iniciando seed...");
+
+  for (let i = 0; i < limit; i++) {
+    const notepad = await createNotepad({
+      title: "Teste Seed 2",
+      Subtitle: "Teste Seed 2",
+      content: "Teste conteúdo seed 2",
+    });
+    console.log(`Notepad criado com id: ${notepad.id}`);
+  }
+
+  console.log("Seed realizado com sucesso!");
+}
+
+notepadSeed();
