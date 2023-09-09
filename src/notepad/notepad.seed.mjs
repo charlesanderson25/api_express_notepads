28a711ffda7e69801234820450bc64b11beb3139
charlesanderson25 <charles_spfc@hotmail.com>
2023-09-09 09:25:46 -0300

    Test create notepad with createdAt

--- src/notepad/notepad.seed.mjs
+++ src/notepad/notepad.seed.mjs
@@ -1,1 +1,16 @@
+import {
+  createNotepad,
+  deleteNotepad,
+  listNotepads,
+  readNotepad,
+  updateNotepad,
+} from "./notepad.model.service.mjs";
+
 console.log("Seed");
+
+createNotepad({
+  title: "Titulo Seed",
+  subtitle: "Subtitulo Seed",
+  content: "Conteúdo test do Seed",
+  createdAt: "2023/09/09",
+});
